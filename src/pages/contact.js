a0e5f20009b074b09056fd5089ac6207973746e0
James McCarthy <jamesmccarthyxci@gmail.com>
2020-02-17 11:14:44 -0500

    Recaptcha

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,12 +8,63 @@ import dark_sky from "../assets/img/project_images/dark_sky/dark_sky.png"
 import expensicon from "../assets/img/project_images/expensicon/expensicon.png"
 import jeopardy from "../assets/img/project_images/jeopardy/jeopardy.png"
 import { GoMarkGithub } from "react-icons/go"
+import M from "materialize-css"
 import Head from "../components/Head"
+
 // https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/
 // https://github.com/sw-yx/gatsby-netlify-form-example-v2
 
-class Projects extends Component {
+const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY
+if (typeof RECAPTCHA_KEY === "undefined") {
+  throw new Error(`
+  Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
+  You probably forget to set it in your Netlify build environment variables. 
+  Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
+  Note this demo is specifically for Recaptcha v2
+  `)
+}
+
+class Contact extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  const recaptchaRef = React.createRef()
+
+  componentDidUpdate = (prevProps, prevState) => {
+    let message = document.getElementById("message")
+    M.textareaAutoResize(message)
+  }
+
+  encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
+
+  handleSubmit = (e, history) => {
+    e.preventDefault()
+    const form = e.target
+    const recaptchaValue = recaptchaRef.current.getValue()
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        'g-recaptcha-response': recaptchaValue,
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value })
+
   render() {
+    const { name, email, message } = this.state
     return (
       <div>
         <Head />
@@ -36,6 +87,7 @@ class Projects extends Component {
                   data-netlify="true"
                   netlify-honeypot="bot-field"
                   data-netlify-recaptcha="true"
+                  onSubmit={this.handleSubmit}
                   action="/thanks"
                 >
                   <div className="hidden">
@@ -49,7 +101,12 @@ class Projects extends Component {
                     <div className="col m10 offset-m1 custom-input-field">
                       <label className="dark-blue-text fw-600">
                         Your Name:
-                        <input type="text" name="name" />
+                        <input
+                          type="text"
+                          name="name"
+                          value={name}
+                          onChange={this.handleChange}
+                        />
                       </label>
                     </div>
                   </div>
@@ -58,7 +115,12 @@ class Projects extends Component {
                     <div className="col m10 offset-m1 custom-input-field">
                       <label className="dark-blue-text fw-600">
                         Your Email:
-                        <input type="email" name="email" />
+                        <input
+                          type="email"
+                          name="email"
+                          value={email}
+                          onChange={this.handleChange}
+                        />
                       </label>
                     </div>
                   </div>
@@ -71,6 +133,8 @@ class Projects extends Component {
                           id="message"
                           name="message"
                           className="materialize-textarea"
+                          value={message}
+                          onChange={this.handleChange}
                         ></textarea>
                       </label>
                     </div>
@@ -78,7 +142,7 @@ class Projects extends Component {
 
                   <div className="row">
                     <div className="col m12">
-                      <div data-netlify-recaptcha="true"></div>
+                      <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} />
                     </div>
                   </div>
 
@@ -102,4 +166,4 @@ class Projects extends Component {
   }
 }
 
-export default Projects
+export default Contact
