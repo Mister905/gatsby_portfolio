bf9d04f3483580ada6dd65d273815e440ad2981e
James McCarthy <jamesmccarthyxci@gmail.com>
2020-02-17 10:50:07 -0500

    Test

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,35 +2,20 @@ import React from "react"
 import { Helmet } from "react-helmet"
 
 const Head = props => {
-  const { title, landing } = props
+    
+  const { title } = props
 
-  if (landing) {
-    return (
-      <Helmet title={`${title}`}>
-        <link
-          href="https://fonts.googleapis.com/css?family=Open+Sans|Permanent+Marker|Tomorrow&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet"
-        />
-        <body className="landing-bg" />
-      </Helmet>
-    )
-  } else {
-    return (
-      <Helmet title={`${title}`}>
-        <link
-          href="https://fonts.googleapis.com/css?family=Open+Sans|Permanent+Marker|Tomorrow&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet"
-        />
-      </Helmet>
-    )
-  }
+  return (
+    <Helmet title={`${title}`}>
+      <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans|Permanent+Marker|Tomorrow&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
+    </Helmet>
+  )
 }
 export default Head
