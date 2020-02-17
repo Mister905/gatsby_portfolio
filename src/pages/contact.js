d64291ae8c560f535cd2c9b9a6125673679cf84a
James McCarthy <jamesmccarthyxci@gmail.com>
2020-02-17 16:02:56 -0500

    Test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -54,6 +54,7 @@ export default function Contact() {
         data-netlify-recaptcha="true"
         onSubmit={handleSubmit}
       >
+        <input type="hidden" name="form-name" value="contact-recaptcha" />
         <noscript>
           <p>This form won’t work with Javascript disabled</p>
         </noscript>
